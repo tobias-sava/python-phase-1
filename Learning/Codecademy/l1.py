dd510bb688879a94d83859be244350a7da218ed0
tobias-sava <tobias.svgb@gmail.com>
2024-12-08 18:43:25 +0200

    push

---
+++ Learning/Codecademy/l1.py
@@ -0,0 +1,18 @@
+# 12/8/2024 - codecademy project "block letters"
+
+# full name
+# fun fact about myself
+
+# this shit is making me create an ascii text BY HAND
+
+ascii = '''
+FFFFF
+F
+F
+FFF
+F
+F
+'''
+print(ascii)
+
+# done
