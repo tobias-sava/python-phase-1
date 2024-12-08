4af6b88bc125a7fdeb0018f363fda9107ea5755a
tobias-sava <tobias.svgb@gmail.com>
2024-12-08 18:51:29 +0200

    LOL

--- Learning/Codecademy/l1.py
+++ Learning/Codecademy/l1.py
@@ -42,7 +42,7 @@ customer_one_itemization += luxurious_lamp_description
 
 customer_one_tax = customer_one_total * sales_tax
 
-customer_one_total += sales_tax
+customer_one_total += customer_one_tax
 
 print("Customer One Items:")
 print(customer_one_itemization)
