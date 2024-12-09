df93bf3f3cb78d023d1ebd07e374bd082ff36e01
tobi <tobias.svgb@gmail.com>
2024-12-09 15:00:59 +0200

    pushy

---
+++ Learning/Codecademy/l4.py
@@ -0,0 +1,21 @@
+# 9/12/2024 - codecademy "introduction to lists - review"
+
+# practicing usage of lists (finished exercises on codecademy)
+
+# personal practice:
+
+# use chatgpt to create a simple project with tasks, avoid direct guidance,
+# use documentation predominantly
+
+# PROJECT NAME HERE
+
+# CODE HERE
+
+
+
+
+
+
+
+
+
