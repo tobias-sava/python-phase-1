a3c42b50e8da6f6c5dcaa64efd7496b47af769a8
tobi <tobias.svgb@gmail.com>
2024-12-12 15:05:16 +0200

    push

---
+++ Learning/Codecademy Py/l8.py
@@ -0,0 +1,43 @@
+# 12/12/2024 - codecademy practice "parameters and arguments"
+
+# i already know a bit about functions but this is a good recap
+
+# creating program that generates directions for user trips
+
+# creating trip instruction function
+
+def generate_trip_instructions(location):
+    print("Looks like you are planning a trip to visit " + location)
+
+generate_trip_instructions("Central Park")
+
+# ok nevermind that was literally the whole exercise ...
+
+# next lesson - multiple parameters
+
+# had to do it on codecademy because i need to use their on-site IDE ...
+
+# built in functions vs user defined functions lesson got me thinking.
+# i now feel like i understand functions better :)
+
+# code from codecademy exercise below
+
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+# Write your code below:
+
+# Checkpoint 1
+max_price = max(tshirt_price, shorts_price, mug_price, poster_price)
+print(max_price)
+
+# Checkpoint 2
+min_price = min(tshirt_price, shorts_price, mug_price, poster_price)
+print(min_price)
+
+# Checkpoint 3
+rounded_price = round(tshirt_price, 1)
+print(rounded_price)
+
